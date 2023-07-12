c45e56ca03a164cd38aac4617902bda0f5bfd832
João Victor <devjoao10@gmail.com>
2023-07-12 09:34:57 -0300

    Primeiro Commit

--- js/main.js
+++ js/main.js
@@ -1,60 +1,60 @@
 function activateMenuAtCurrentSection(section) {
-  const targetLine = scrollY + innerHeight / 2
+  const targetLine = scrollY + innerHeight / 2;
 
   // verificar se a seção passou da linha
   // quais dados vou precisar?
-  const sectionTop = section.offsetTop
-  const sectionHeight = section.offsetHeight
-  const sectionTopReachOrPassedTargetline = targetLine >= sectionTop
+  const sectionTop = section.offsetTop;
+  const sectionHeight = section.offsetHeight;
+  const sectionTopReachOrPassedTargetline = targetLine >= sectionTop;
 
   // verificar se a base está abaixo da linha alvo
 
-  const sectionEndsAt = sectionTop + sectionHeight
-  const sectionEndPassedTargetline = sectionEndsAt <= targetLine
+  const sectionEndsAt = sectionTop + sectionHeight;
+  const sectionEndPassedTargetline = sectionEndsAt <= targetLine;
 
   // limites da seção
   const sectionBoundaries =
-    sectionTopReachOrPassedTargetline && !sectionEndPassedTargetline
+    sectionTopReachOrPassedTargetline && !sectionEndPassedTargetline;
 
-  const sectionId = section.getAttribute('id')
-  const menuElement = document.querySelector(`.menu a[href*=${sectionId}]`)
+  const sectionId = section.getAttribute("id");
+  const menuElement = document.querySelector(`.menu a[href*=${sectionId}]`);
 
-  menuElement.classList.remove('active')
+  menuElement.classList.remove("active");
   if (sectionBoundaries) {
-    menuElement.classList.add('active')
+    menuElement.classList.add("active");
   }
 }
 
-const navigation = document.querySelector('#navigation')
+const navigation = document.querySelector("#navigation");
 
 function showNavOnScroll() {
   if (scrollY > 0) {
-    navigation.classList.add('scroll')
+    navigation.classList.add("scroll");
   } else {
-    navigation.classList.remove('scroll')
+    navigation.classList.remove("scroll");
   }
 }
 
 function showBackToTopButtonOnScroll() {
   if (scrollY > 550) {
-    backToTopButton.classList.add('show')
+    backToTopButton.classList.add("show");
   } else {
-    backToTopButton.classList.remove('show')
+    backToTopButton.classList.remove("show");
   }
 }
 
 function openMenu() {
-  document.body.classList.add('menu-expanded')
+  document.body.classList.add("menu-expanded");
 }
 
 function closeMenu() {
-  document.body.classList.remove('menu-expanded')
+  document.body.classList.remove("menu-expanded");
 }
 
 const swiper = new Swiper(".mySwiper", {
   slidesPerView: 1,
   autoplay: {
-    delay: 5000
+    delay: 5000,
   },
   pagination: {
     el: ".swiper-pagination",
@@ -65,17 +65,16 @@ const swiper = new Swiper(".mySwiper", {
   breakpoints: {
     767: {
       slidesPerView: 3,
-      setWrapperSize: true
-    }
-  }
-})
+      setWrapperSize: true,
+    },
+  },
+});
 
 const swiper1 = new Swiper(".Swiper-content", {
   effect: "coverflow",
   grabCursor: true,
   centeredSlides: true,
   slidesPerView: "auto",
-  loop: true,
   coverflowEffect: {
     rotate: 0,
     stretch: 0,
@@ -89,17 +88,15 @@ const swiper1 = new Swiper(".Swiper-content", {
     clickable: true,
   },
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
   },
 });
 
-
-
 ScrollReveal({
-  origin: 'top',
-  distance: '30px',
-  duration: 700
+  origin: "top",
+  distance: "30px",
+  duration: 700,
 }).reveal(`
   #home, 
   #home img, 
@@ -109,20 +106,18 @@ ScrollReveal({
   #services .card
   #about, 
   #about header, 
-  #about .content`)
+  #about .content`);
 
-
-
-window.addEventListener('scroll', onScroll)
+window.addEventListener("scroll", onScroll);
 function onScroll() {
-  showNavOnScroll()
-  showBackToTopButtonOnScroll()
+  showNavOnScroll();
+  showBackToTopButtonOnScroll();
 
-  activateMenuAtCurrentSection(home)
-  activateMenuAtCurrentSection(about)
-  activateMenuAtCurrentSection(services)
-  activateMenuAtCurrentSection(portfolio)
-  activateMenuAtCurrentSection(contact)
+  activateMenuAtCurrentSection(home);
+  activateMenuAtCurrentSection(about);
+  activateMenuAtCurrentSection(services);
+  activateMenuAtCurrentSection(portfolio);
+  activateMenuAtCurrentSection(contact);
 }
 
-onScroll()
+onScroll();
